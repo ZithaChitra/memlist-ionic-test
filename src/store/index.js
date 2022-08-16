786889bfb2ff61abed332d01a4a244e3b7bbfd8e
zithachitra <69644637+ZithaChitra@users.noreply.github.com>
2022-08-16 13:48:14 +0200

    init setup

---
+++ src/store/index.js
@@ -0,0 +1,47 @@
+import { createStore } from 'vuex'
+
+
+import sea from '/public/assets/images/sea.jpg'
+import mnt from '/public/assets/images/mnt.jpg'
+import food from '/public/assets/images/food.jpg'
+
+const store = createStore({
+    state(){
+        return {
+            memories: [
+                {
+                    id: "m1",
+                    image: mnt,
+                    title: "A trip into the mountains",
+                    description: "It was a really nice trip"
+                },
+                {
+                    id: "m2",
+                    image: sea,
+                    title: "Surfing the sea side",
+                    description:"Feeling the cool breeze" 
+                },
+                {
+                    id: "m3",
+                    image: food,
+                    title: "Good eating",
+                    description: "Really tasty !!"
+                },
+            ],
+
+        }
+    },
+    getters: {
+        memories(state){
+            return state.memories;
+        },
+        memory(state){
+            return (memoryId) => {
+                return state.memories.find(memory => memory.id === memoryId);
+            }
+        }
+    }
+});
+
+
+export default store;
